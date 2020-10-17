2b99af402a4a90735b5edfce2e7429c4b30f9d72
crow1011 <crowsmith1011>
2020-10-17 13:05:21 +0300

    add new docker-compose for get_my_id

--- app/get_my_id.py
+++ app/get_my_id.py
@@ -8,7 +8,7 @@ logger = get_logger(logger_conf)
 bot = telebot.TeleBot(tg_conf['api_key'])
 
 
-@bot.message_handler(commands=['my_id'])
+@bot.message_handler(commands=['my_id', 'get_my_id'])
 def my_id(message):
     try:
         bot.send_message(message.chat.id, 'Sending your chat id...')
--- app/main.py
+++ app/main.py
@@ -19,12 +19,12 @@ vk_api = vk.API(session)
 es = elasticsearch.Elasticsearch(conf['es']['host'])
 bot = telebot.TeleBot(conf['tg']['api_key'])
 
-def get_adm_count(group_id, vk_api):
+def get_adm_list(group_id, vk_api):
     res = vk_api.groups.getMembers(group_id=group_id, v='5.124', filter=['manages'])
     return res
 
 
-def check_adm_count_change(group_id, adm_count, index):
+def check_adm_list_change(group_id, adm_list, index):
     change_status = None
     res = es.search(index=index, body={
         "size": 1,
@@ -35,26 +35,63 @@ def check_adm_count_change(group_id, adm_count, index):
     })
     if res['hits']['total']['value']==0:
         logger.debug('# index or group_id not found')
-    elif res['hits']['hits'][0]['_source']['adm_count']!=adm_count:
+        # stop and return None if it's first start
+        return change_status, None
+    es_adm_items = res['hits']['hits'][0]['_source']['adm_items']
+    if sorted(es_adm_items) != sorted(adm_list['items']):
         change_status = True
-        last_count = res['hits']['hits'][0]['_source']['adm_count']
-        logger.debug(f'# fix change adm_count last:{last_count} != actual {adm_count}')
-    elif res['hits']['hits'][0]['_source']['adm_count']==adm_count:
+        logger.debug(f'# fix change adm_list last:{es_adm_items} <> actual {adm_list}')
+    else:
         change_status = False
-        last_count = res['hits']['hits'][0]['_source']['adm_count']
-        logger.debug(f'# adm_count no change. Last count {last_count}')
-    return change_status
+        logger.debug(f'# adm_list no change. Last list {es_adm_items}')
+    return change_status, es_adm_items
 
 
-def send_to_es(group_id, adm_count, index, alerter):
+def send_to_es(group_id, adm_list, index, alerter):
     doc = {
         'alerter': str(alerter),
         'group_id': f"{str(group_id)}",
-        'adm_count': adm_count,
+        'adm_count': adm_list['count'],
+        'adm_items': adm_list['items'],
         '@timestamp': datetime.utcnow(),
     }
     res = es.index(index=index, body=doc)
     return res
+
+def msg_gen(vk_api, group_id, adm_items, es_adm_items):
+    group_name = vk_api.groups.getById(group_ids=group_id, v='5.124')[0]['name']
+    msg = f'found a change in the list of admins for the {group_name}({group_id}) group\n'
+    last_adm = vk_api.users.get(user_ids=es_adm_items, v='5.124')
+    now_adm = vk_api.users.get(user_ids=adm_items,  v='5.124')
+    msg += 'Last admins list:\n'
+    for adm in last_adm:
+        try:
+            adm_first_name = adm["first_name"]
+        except:
+            adm_first_name = ''
+        try:
+            adm_last_name = adm["last_name"]
+        except:
+            adm_last_name = ''
+
+        msg += f' - {adm_first_name} {adm_last_name}(user_id: {adm["id"]})'
+    msg += '\nNow admins list:\n'
+    for adm in now_adm:
+        try:
+            adm_first_name = adm["first_name"]
+        except:
+            adm_first_name = ''
+        try:
+            adm_last_name = adm["last_name"]
+        except:
+            adm_last_name = ''
+
+        msg += f' - {adm_first_name} {adm_last_name}(user_id: {adm["id"]})'
+    print(last_adm)
+    print(now_adm)
+    print(msg)
+    return msg
+
 
 def send_alert(chat_id, msg):
     bot.send_message(int(chat_id), msg)
@@ -62,20 +99,21 @@ def send_alert(chat_id, msg):
 
 def main():
     group_id = conf['vk']['group_id']
-    adm_count = get_adm_count(group_id, vk_api)['count']
-    logger.debug(f'# adm_count: {adm_count} for group_id{group_id}')
+    adm_list = get_adm_list(group_id, vk_api)
+    logger.debug(f'# adm_list: {adm_list} for group_id{group_id}')
     e_index = conf['es']['index']
     k_index = e_index + '*'
     alerter = conf['server']['name']
-    adm_count_change = check_adm_count_change(group_id, adm_count,k_index)
-    adm_count_change = True
-    if adm_count_change:
-        msg = f"Changing adm_count for group_id: {str(adm_count)}. Now count: {str(adm_count)}"
+    adm_list_change, es_adm_items = check_adm_list_change(group_id, adm_list, k_index)
+    print(es_adm_items)
+    adm_list_change = True
+    if adm_list_change:
+        msg = msg_gen(vk_api, group_id, adm_list['items'], es_adm_items)
         send_alert(conf['tg']['chat_id'], msg)
-        res = send_to_es(group_id, adm_count, e_index, alerter)
+        res = send_to_es(group_id, adm_list, e_index, alerter)
     else:
         if conf['server']['fix_no_change']:
-            res = send_to_es(group_id, adm_count, e_index, alerter)
+            res = send_to_es(group_id, adm_list, e_index, alerter)
     return True
 
 
